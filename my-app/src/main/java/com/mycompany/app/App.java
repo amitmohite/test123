dbce9b740fed651171c380ca949303caf45a6783
amitm3717@gmail.com <amitm3717@gmail.com>
2018-04-04 15:41:49 +0530

    modified main class

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "My Hello World!" );
+        System.out.println( "Testing My Hello World!" );
     }
 }
